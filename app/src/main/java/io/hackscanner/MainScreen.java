0389c3df8cc0f23f20eeb351106335051382922d
Dariusz <darekshakur@gmail.com>
2016-10-08 21:22:55 +0200

    MLH dates

--- app/src/main/java/io/hackscanner/MainScreen.java
+++ app/src/main/java/io/hackscanner/MainScreen.java
@@ -70,8 +70,8 @@ public class MainScreen extends AppCompatActivity {
                 /*title = document.title();*/
                 Elements cities = document.body().select("span[itemprop=addressLocality]");
                 Elements countries = document.body().select("span[itemprop=addressRegion]");
-                /*Elements startDates = document.body().select("span[itemprop=startDate]");
-                Elements endDates = document.body().select("span[itemprop=endDate]");*/
+                Elements startDates = document.body().select("meta[itemprop=startDate]");
+                Elements endDates = document.body().select("meta[itemprop=endDate]");
                 Elements names = document.body().select("h3[itemprop=name]");
 
 
@@ -79,18 +79,18 @@ public class MainScreen extends AppCompatActivity {
                 for(int i=0; i<cities.size();i++){
                     citiesArray.add(cities.get(i).ownText());
                     countriesArray.add(countries.get(i).ownText());
-                    /*startDatesArray.add(startDates.get(i).ownText());
-                    endDatesArray.add(endDates.get(i).ownText());*/
+                    startDatesArray.add(startDates.get(i).attr("content"));
+                    endDatesArray.add(endDates.get(i).attr("content"));
                     namesArray.add(names.get(i).ownText());
                 }
 
                 //data per hackathon
                 for(int i=0; i<cities.size();i++){
                     List<String> hackathonData = new ArrayList<String>();
-                    hackathonData.add(citiesArray.get(i));
-                    hackathonData.add(countriesArray.get(i));
-                    /*hackathonData.add(startDatesArray.get(i));
-                    hackathonData.add(endDatesArray.get(i));*/
+                    hackathonData.add("City: "+citiesArray.get(i));
+                    hackathonData.add("Country: "+countriesArray.get(i));
+                    hackathonData.add("Start date: "+startDatesArray.get(i));
+                    hackathonData.add("End date: " +endDatesArray.get(i));
 
                     listDataChild.put(namesArray.get(i), hackathonData);
 
