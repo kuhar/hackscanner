9f2ec82f02e2431acfe066041211cdf948fc9caf
Dariusz <darekshakur@gmail.com>
2016-10-08 14:26:14 +0200

    Get places of hackathons

--- app/src/main/java/io/hackscanner/MainScreen.java
+++ app/src/main/java/io/hackscanner/MainScreen.java
@@ -1,31 +1,81 @@
 package io.hackscanner;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainScreen extends AppCompatActivity {
+
+    String url = "https://mlh.io/seasons/eu-2017/events";
+    List<String> citiesArray = new ArrayList<>();
+    List<String> countriesArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Button titlebutton = (Button) findViewById(R.id.button);
+
+        titlebutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Execute Title AsyncTask
+                new Title().execute();
             }
         });
+
+    }
+
+    // Title AsyncTask
+    private class Title extends AsyncTask<Void, Void, Void> {
+        String title;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.connect(url).get();
+                // Get the html document title
+                /*title = document.title();*/
+                Elements cities = document.body().select("span[itemprop=addressLocality]");
+                Elements countries = document.body().select("span[itemprop=addressRegion]");
+
+                for(int i=0; i<cities.size();i++){
+                    citiesArray.add(cities.get(i).ownText());
+                    countriesArray.add(cities.get(i).ownText());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set title into TextView
+            TextView txttitle = (TextView) findViewById(R.id.textView);
+            
+
+        }
     }
 
     @Override
@@ -48,4 +98,10 @@ public class MainScreen extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
+
+
+
+
